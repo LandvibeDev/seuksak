ae76543d940b0ad025bac59ee4977f623ca6d00f
sungon lee <dltjddhs@gmail.com>
2017-01-02 19:52:26 +0900

    ADD - project_add.ejs project_name, create_date

--- routes/project_add.js
+++ routes/project_add.js
@@ -4,7 +4,8 @@
 var express = require('express');
 var router = express.Router();
 var connection = require('./connection'); // db connection 모듈을 불러온다.
-var node_git = require("nodegit");             // nodegit 모듈
+var node_git = require("nodegit");        // nodegit 모듈
+var date = require('date-utils');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,11 +15,16 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next){
     var git_url = req.body.git_url; // "https://github.com/LandvibeDev/seuksak/"
+    var project_name = req.body.project_name;
+    var present_time = new Date();
+    var datetime = present_time.toFormat("YYYYMMDD HH24MISS");
+    var query_datetime = 'STR_TO_DATE(\'' + datetime +'\', \'%Y%m%d %H:%i:%s\'';
 
     // git_url 에 있는 프로젝트를 clone
     // group_id, project_id 반영 필요
     // clone 할 project 에 대해 중복 검사 필요
-    var local_path = "../seuksak_workspace/group/1/project/1/src/";
+    // var local_path = "../seuksak_workspace/group/1/project/1/src/";
+    var local_path = "../seuksak_workspace/group/1/project/2/src/";
 
     var clone_options = {};
     clone_options.fetchOpts = {
@@ -38,8 +44,8 @@ router.post('/', function(req, res, next){
         })
     */
     // clone 후 db에 반영
-    var query = connection.query('INSERT INTO Project (group_id, git_url) VALUES (?, ?)'
-        ,[1, git_url]
+    var query = connection.query('INSERT INTO Project (group_id, git_url, project_name, create_date) VALUES (?, ?, ?, ?)'
+        ,[1, git_url, project_name, query_datetime]
         ,function(error, result){
             if(error){
                console.log(error);
