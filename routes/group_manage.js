7abe36d468dfcbc2d3cbfdc59051073afee25e70
GuBeomJeong <jgb625@naver.com>
2017-01-02 23:20:38 +0900

    UPDATE - group 추가 add

--- routes/group_manage.js
+++ routes/group_manage.js
@@ -5,18 +5,36 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var connection = require('./connection');
+var date = require('date-utils');
 
-/* GET home page. */
+var _result;
 router.get('/', function(req, res, next) {
 
+    console.log(req);
     connection.query('SELECT * FROM GroupMember WHERE user_id = ?',
         [req.user.id],function(error,result){
             if(error){
                 console.log(error);
             }
             console.log(result);
-            res.render('group_manage',{groups:result});
+            _result = result;
+            res.render('group_manage',{groups:result,dup:false});
         });
+});
+
+router.post('/', function(req, res, next){
+    console.log("check");
+    var presenttime = new Date();
+    var present = presenttime.toFormat("YYYY-MM-DD HH24:MI:SS");
+    console.log(present);
+    var groupname = req.body.groupname;
+    connection.query('INSERT INTO seuksak.Group (create_date, groupname) VALUES (?, ?)',[present, groupname], function(err){
+        if(err){
+            res.render('group_manage',{groups:_result,dup:true});
+        }else{
+            res.redirect('/group');
+        }
+    });
 });
 
 router.get('/:id',function(req,res,next){
