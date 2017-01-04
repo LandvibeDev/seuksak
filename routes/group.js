116b8b2829166a77d5788ff0f74f5505bd642bef
sungon lee <dltjddhs@gmail.com>
2017-01-04 22:21:37 +0900

    UPDATE - file download

--- routes/group.js
+++ routes/group.js
@@ -7,7 +7,7 @@ var passport = require('passport');
 var connection = require('./connection');
 var date = require('date-utils');
 var node_git = require("nodegit");        // nodegit 모듈
-var group_member_list;
+var http = require('http');
 
 var node_git = require("nodegit");
 var multipart = require('connect-multiparty');   // file upload
@@ -242,6 +242,22 @@ router.get('/:group_id/project/:project_id/build/:build_id', function(request, r
 
 // 그룹/프로젝트/빌드/빌드결과 다운로드
 router.get('/:group_id/project/:project_id/build/:build_id/download', function(request, response, next){
+    var build_id = request.params.build_id;
+    var group_id = request.params.group_id;
+    var project_id = request.params.project_id;
+    connection.query('SELECT * FROM seuksak.Project where id = ?',[project_id],function(error,result){
+        var download_path = __dirname + '/../../seuksak_workspace/group/'+group_id+'/project/'+project_id+'/src/'+result[0].result_file_name;
+
+        fs.readFile(download_path, 'binary', function (error, data) {
+            response.writeHead(200,
+                {'Content-Type' : 'binary',
+                    'Content-Disposition':'attachment;filename=' + result[0].result_file_name
+                });
+            response.write(data);
+            //response.redirect('/group/' + group_id + '/project/' + project_id + '/build/'+build_id);
+            response.end();
+        });
+    });
 
 });
 
